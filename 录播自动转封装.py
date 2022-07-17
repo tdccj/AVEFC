4a0f205e380ce945809877869c1edf87d5c894a9
tdccj <tudouccj@163.com>
2022-07-18 03:30:50 +0800

    v2.0.b

--- 录播自动转封装.py
+++ 录播自动转封装.py
@@ -1,6 +1,7 @@
 # coding = utf-8
 # 录播自动转封装v1.0 @tdccj
 # 暂时可以运行，录播质量有无问题未知，待开发备份功能
+# 代码重构中
 
 from watchdog.events import FileSystemEventHandler
 
@@ -15,25 +16,56 @@ import os
 
 print("os装载成功")
 
-cwd = os.getcwd()
 
-try:
-    zm = open(cwd + "\\录播自动转封装设置.zm", "r")
-    The_Path = zm.readline()
-    print(The_Path)
-    zm.close()
+def main():
+    cwd = os.getcwd()  # 获取当前目录
+    The_Path = read(cwd)
+    Time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    lu_jing = "创建变量"
+    if __name__ == "__main__":  # 用observe对目录进行检测
+        path = The_Path
+        event_handler = Watch()
+        observer = Observer()
+        try:
+            observer.schedule(event_handler, path, recursive=True)
+            observer.start()
+            print("监控运行")
+        except FileNotFoundError:
+            zm = open(cwd + "\\录播自动转封装设置.zm", "w")
+            The_Path = input("输入路径2")
+            zm.write(The_Path)
+            zm.close()
+            try:
+                path = The_Path
+                event_handler = Watch()
+                observer = Observer()
+                observer.schedule(event_handler, path, recursive=True)
+                observer.start()
+            except FileNotFoundError:
+                print("路径仍然错误，请重启")
+        try:
+            while True:  # 用来保证程序持续执行
+                time.sleep(1)
 
-except FileNotFoundError:
-    zm = open(cwd + "\\录播自动转封装设置.zm", "w")
-    The_Path = input("输入路径")
-    zm.write(The_Path)
-    zm.close()
+        except KeyboardInterrupt:  # 除了程序被用户中断
+            observer.stop()
 
-print("监控中")
 
-Time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+def read(cwd):
+    try:
+        zm = open(cwd + "\\录播自动转封装设置.zm", "r")
+        The_Path = zm.readline()
+        print(The_Path)
+        zm.close()
 
-lu_jing = "创建变量"
+    except FileNotFoundError:
+        zm = open(cwd + "\\录播自动转封装设置.zm", "w")
+        The_Path = input("输入路径")
+        zm.write(The_Path)
+        zm.close()
+
+    print("监控中")
+    return The_Path
 
 
 def zhuan_ma(lu_jing):  # 用来执行转封装操作
@@ -49,6 +81,7 @@ def zhuan_ma(lu_jing):  # 用来执行转封装操作
 
 class Watch(FileSystemEventHandler):  # 用来接受events的反馈
     def on_modified(self, event):
+        Time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         print(f'文件修改:{event.src_path}', Time)
         lu_jing = str(event.src_path)
 
@@ -57,30 +90,5 @@ class Watch(FileSystemEventHandler):  # 用来接受events的反馈
             zhuan_ma(lu_jing)
 
 
-if __name__ == "__main__":  # 用observe对目录进行检测
-    path = The_Path
-    event_handler = Watch()
-    observer = Observer()
-    try:
-        observer.schedule(event_handler, path, recursive=True)
-        observer.start()
-        print("监控运行")
-    except FileNotFoundError:
-        zm = open(cwd + "\\录播自动转封装设置.zm", "w")
-        The_Path = input("输入路径2")
-        zm.write(The_Path)
-        zm.close()
-        try:
-            path = The_Path
-            event_handler = Watch()
-            observer = Observer()
-            observer.schedule(event_handler, path, recursive=True)
-            observer.start()
-        except FileNotFoundError:
-            print("路径仍然错误，请重启")
-    try:
-        while True:  # 用来保证程序持续执行
-            time.sleep(1)
-
-    except KeyboardInterrupt:  # 除了程序被用户中断
-        observer.stop()
+if __name__ == "__main__":
+    main()
